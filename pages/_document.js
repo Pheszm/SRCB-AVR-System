e6044d4c3b30f1e6cc03918eaacab8b7d817d6bc
Pheszm <122964651+Pheszm@users.noreply.github.com>
2025-02-23 23:35:15 +0800

    Initial commit

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="./App_Icon.png" type="image/png" />
+        <link href="./googlefont_Poppins.css" rel="stylesheet" />
+
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import LoginForm from "./loginform";
+
+export default function Home() {
+
+  return (
+    <div>
+    <title>SRCB AVR | Login Page</title>
+    <LoginForm/>
+    </div>
+  );
+}
