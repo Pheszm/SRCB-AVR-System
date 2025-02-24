c784fed292246c6ee935f968ab7b45aed9392501
Pheszm <122964651+Pheszm@users.noreply.github.com>
2025-02-24 08:48:33 +0800

    adds '/' in url fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: false, // This will remove the trailing slash on how it adds '/' in github
 };
 
 export default nextConfig;
